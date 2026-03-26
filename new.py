c0647193a3113ebd3b51fd065325945aee44e2e2
Np-creator <66473500+Np-creator@users.noreply.github.com>
2026-03-26 02:04:54 +0100

    needs debugging

--- new.py
+++ new.py
@@ -1,1 +1,22 @@
-print("heeloo")
+guess_name = input("enter a name: ")
+
+name1 = "Alex"
+name2 = "brian"
+name3 = "Jash"
+
+attempts = 3
+
+while attempts > 0:
+    if guess_name not in [name1, name2, name3]:
+        print(guess_name)
+        attempts -= 1
+        print("Take another guess")
+    else:
+        break
+
+else:
+    print("bye")
+
+
+
+#This code needs to be debugged tomorrowWWW
