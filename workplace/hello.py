b3abc95302fbbc9b96a996fc1c4bb7471a77a975
Np-creator <66473500+Np-creator@users.noreply.github.com>
2026-04-06 17:25:27 +0200

    updated

--- workplace/hello.py
+++ workplace/hello.py
@@ -1,5 +1,5 @@
 def greetings(**user):
     first_name = user.get("first_name", "Anonymous")
     last_name = user.get("last_name", 13)
-    print(f"\033[31m Hello {first_name} {last_name} Welcome to hell!")
+    print(f"\033[31m Hello {first_name} {last_name} Welcome to hell!\033[31m")
 greetings(last_name = "313")
