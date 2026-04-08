7615ec850aef4740d0dcc8d46c46ca6b8b37495f
Np-creator <66473500+Np-creator@users.noreply.github.com>
2026-04-08 14:39:15 +0200

    updated

--- workplace/hello.py
+++ workplace/hello.py
@@ -3,3 +3,17 @@ def greetings(**user):
     last_name = user.get("last_name", 13)
     print(f"\033[31m Hello {first_name} {last_name} Welcome to hell!\033[31m")
 greetings(last_name = "313")
+
+def calculator(*num):
+    return num
+print(calculator(2,34,0))
+
+def addition(a,b):
+    sum = a + b 
+    return sum  
+
+def input(c):
+     c = input = print(int(("Give a number: ")))
+     return c 
+
+print(addition(2,6) + input) 
