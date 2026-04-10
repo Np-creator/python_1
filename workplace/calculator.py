cb44df526662cd93e1e72e96c749519e7b0d9e8f
Np-creator <66473500+Np-creator@users.noreply.github.com>
2026-04-10 15:50:29 +0200

    import

---
+++ workplace/calculator.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    summe = a + b
+    return summe 
+
+def min(a,b):
+    subraction = a - b 
+    return subraction 
+
+def multi(a,b): 
+    multiplication = a * b  
+    return multiplication 
+
--- workplace/hello.py
+++ workplace/hello.py
@@ -1,19 +1,6 @@
-def greetings(**user):
-    first_name = user.get("first_name", "Anonymous")
-    last_name = user.get("last_name", 13)
-    print(f"\033[31m Hello {first_name} {last_name} Welcome to hell!\033[31m")
-greetings(last_name = "313")
+import calculator
 
-def calculator(*num):
-    return num
-print(calculator(2,34,0))
+result = calculator.multi (2,10)
 
-def addition(a,b):
-    sum = a + b 
-    return sum  
+print(result)
 
-def input(c):
-     c = input = print(int(("Give a number: ")))
-     return c 
-
-print(addition(2,6) + input) 
